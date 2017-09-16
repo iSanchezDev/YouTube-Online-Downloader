32fd07cef47558c6c80a8036e209c083b69fc901
Iván Sánchez <isancheznetwork@gmail.com>
2017-09-16 19:42:02 +0200

    Client basic steps done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from "./home/home.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularMaterialModule} from "./tools/modules/angular-material.module";
+import {DownloaderComponent} from './home/downloader/downloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DownloaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/tools/modules/angular-material.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdInputModule,
+  MdMenuModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdSlideToggleModule,
+  MdTabsModule,
+  MdTooltipModule,
+  MdPaginatorModule,
+  MdTableModule,
+  MdCardModule,
+  MdIconModule,
+  MdToolbarModule,
+  MdChipsModule,
+  MdProgressBarModule,
+  MdExpansionModule
+} from "@angular/material";
+
+@NgModule({
+  imports: [
+    MdButtonModule,
+    MdRadioModule,
+    MdCheckboxModule,
+    MdProgressSpinnerModule,
+    MdTabsModule,
+    MdTooltipModule,
+    MdInputModule,
+    MdSlideToggleModule,
+    MdTableModule,
+    MdPaginatorModule,
+    MdMenuModule,
+    MdCardModule,
+    MdIconModule,
+    MdToolbarModule,
+    MdChipsModule,
+    MdProgressBarModule,
+    MdExpansionModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdRadioModule,
+    MdCheckboxModule,
+    MdProgressSpinnerModule,
+    MdTabsModule,
+    MdTooltipModule,
+    MdInputModule,
+    MdSlideToggleModule,
+    MdTableModule,
+    MdPaginatorModule,
+    MdMenuModule,
+    MdCardModule,
+    MdIconModule,
+    MdToolbarModule,
+    MdChipsModule,
+    MdProgressBarModule,
+    MdExpansionModule
+  ],
+  declarations: [],
+  providers: [],
+})
+export class AngularMaterialModule {}
